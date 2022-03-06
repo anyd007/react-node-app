f69a4fbcc71fb6dc4f0fd61bcaedfb062e299e1a
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-06 18:49:18 +0100

    integracja heroku z mongodb

--- server/index.js
+++ server/index.js
@@ -34,9 +34,9 @@ mongoose
     app.use(express.json());
     app.use(favicon(__dirname + '/build/favicon.ico'))
     app.use(express.static(path.join(__dirname, 'build')))
-//   app.get('*', (req, res) => {
-//   res.sendFile( path.join(__dirname + '/build/index.html'))
-// })
+  app.get('*', (req, res) => {
+  res.sendFile( path.join(__dirname + '/build/index.html'))
+})
     app.use("/api", routes); //app bedzie uzywać /api oraz odniesienie do pliku routers
 
     //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
