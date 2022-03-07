17e07f1f99430eeb6debcd257694a16016811ed6
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-07 11:03:35 +0100

    zmiana struktur folderow

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
      
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/**', { target: 'https://react-node-app-db.herokuapp.com' }));
+    app.use(createProxyMiddleware('/api/**', { target: 'http://localhost:3001' }));
 
 };
 
--- server/index.js
+++ server/index.js
@@ -33,14 +33,12 @@ mongoose
     app.use(cors()); //dalsza walka z cors
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-    app.use(favicon(__dirname + '/build/favicon.ico'))
-    if (process.env.NODE_ENV !== 'development') {
-      app.use(express.static('public'))}
-    // app.use(express.static(path.join(__dirname, 'build')))
-//   app.get('*', (req, res) => {
-//   res.sendFile( path.join(__dirname + '/build/index.html'))
-// })
     app.use("/api", routes); //app bedzie uzywać /api oraz odniesienie do pliku routers
+    app.use(favicon(__dirname + '/build/favicon.ico'))
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/index.html'));
+    });
 
     //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
     const herokuPort = process.env.PORT || 3001;
